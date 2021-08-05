e40ffb1c99090842ae0823d53138d2850440388f
NickDayFSD <Nick.Day.FSD@gmail.com>
2021-08-04 17:31:43 -0700

    Character List built

---
+++ src/components/characters/CharacterList.jsx
@@ -0,0 +1,39 @@
+import React, { useState } from 'react';
+import { Link } from 'react-router-dom';
+import { useCharacters } from '../../state/char';
+
+const CharacterList = () => {
+  const [page, setPage] = useState(1);
+  const { characters, loading } = useCharacters;
+  if (loading)
+    return (
+      <img
+        src="https://media.giphy.com/media/jyanYxvGz3MOc/giphy.gif"
+        alt="Loading"
+      />
+    );
+
+  const characterElements = characters.map((character) => (
+    <li key={character.id}>
+      <Link to={`/${character.id}`}>
+        <p>Character Here</p>
+      </Link>
+    </li>
+  ));
+
+  return (
+    <>
+      <button
+        disabled={page <= 1}
+        onClick={() => setPage((prevPage) => prevPage - 1)}
+      >
+        &lt;
+      </button>
+      {page}
+      <button onClick={() => setPage((prevPage) => prevPage + 1)}>&gt;</button>
+      <ul>{characterElements}</ul>
+    </>
+  );
+};
+
+export default CharacterList;
